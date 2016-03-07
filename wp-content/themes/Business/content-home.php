b4405a0b11b9eb6efb09b6c549b9fb684e4a771d
Alex Gutierrez <alex.techuba@gmail.com>
2016-03-07 15:23:10 -0800

    getting the hang of this theme and functional gulp file

--- wp-content/themes/Business/content-home.php
+++ wp-content/themes/Business/content-home.php
@@ -7,7 +7,7 @@
 	<div class="col-1-3">
 		<div class="home-blog">
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            
+
 				<header class="home-entry-header">
     				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('home-blog'); ?></a>
 						<div class="home-entry-meta">
@@ -16,16 +16,16 @@
 					<a href="<?php the_permalink(); ?>"><?php the_title( '<h1 class="home-entry-title">', '</h1>' ); ?></a>
 				</header><!-- .entry-header -->
 
-					<div class="home-entry-content"> 
+					<div class="home-entry-content">
 						<?php the_excerpt(); ?>
 					</div><!-- .entry-content -->
 
     			<a href="<?php the_permalink(); ?>">
 				<footer class="entry-footer">
 					<p>Read Full Article<span><i class="fa fa-long-arrow-right"></i></span></p>
-				</footer><!-- .entry-footer -->	
+				</footer><!-- .entry-footer -->
                 </a>
-                   
+
 			</article><!-- #post-## -->
 		</div><!-- home-blog -->
-	</div><!-- col-1-3 --> 
+	</div><!-- col-1-3 -->
